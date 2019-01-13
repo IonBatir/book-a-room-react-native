f895ada4b1b1c6844a789ef52a29cdd602f15b2b
Ion Batîr <ionbatir@gmail.com>
2019-01-14 01:02:21 +0200

    Add contst

--- src/const.js
+++ src/const.js
@@ -1,3 +1,6 @@
+export const APP_NAME = "Book A Room";
 export const HOTELS_SCREEN = "Hotels";
 export const ROOMS_SCREEN = "Rooms";
 export const REVIEWS_SCREEN = "Reviews";
+export const BOOK_SCREEN = "Book";
+export const ROOMS_LIST_SCREEN = "RoomsList";
